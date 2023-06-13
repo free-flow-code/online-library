ee32250ec8825b899e19a94c66ba4432d891d441
free-flow-code <wildmuskaria8@gmail.com>
2023-06-13 21:39:42 +0400

    Added book missing handler.

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import requests
 from pathlib import Path
 
 
+def check_for_redirect(response):
+    if response.url == 'https://tululu.org/':
+        raise requests.exceptions.HTTPError
+
+
 def download_book(url):
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
     return response.content
 
 
@@ -19,9 +25,12 @@ def main():
     url_template = 'https://tululu.org/txt.php?id='
     for i in range(1, 11):
         url = url_template + str(i)
-        book = download_book(url)
-        filename = f'id{i}.txt'
-        save_book(book, filename)
+        try:
+            book = download_book(url)
+            filename = f'id{i}.txt'
+            save_book(book, filename)
+        except requests.exceptions.HTTPError:
+            pass
 
 
 if __name__ == '__main__':
