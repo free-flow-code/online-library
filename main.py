46bc2b9047c0ceead05e4c3a95cfae20fe3080ac
free-flow-code <wildmuskaria8@gmail.com>
2023-06-13 22:46:12 +0400

    Added the function to get the title and author of the book.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
 
 
 def check_for_redirect(response):
@@ -33,5 +34,19 @@ def main():
             pass
 
 
+def print_book_data():
+    url = 'https://tululu.org/b1/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_header_content = soup.find('body').find('table').find('h1')
+    book_header_text = book_header_content.text
+    book_title = book_header_text.split('::')[0].strip()
+    book_author = book_header_text.split('::')[1].strip()
+    print(f'Заголовок: {book_title}')
+    print(f'Автор: {book_author}')
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    print_book_data()
