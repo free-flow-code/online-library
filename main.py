3758f1899575ad25d8475cc1e8bde60bc91a47f1
free-flow-code <wildmuskaria8@gmail.com>
2023-06-14 12:39:54 +0400

    Download book covers.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit
 
 
 def check_for_redirect(response):
@@ -10,14 +11,16 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def get_book_title(url):
+def get_book_details(url):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     book_header_content = soup.find('body').find('table').find('h1')
     book_header_text = book_header_content.text
     book_title = book_header_text.split('::')[0].strip()
-    return book_title
+    image_src = soup.select_one('div.bookimage img')['src']
+    image_url = urljoin(url, image_src)
+    return book_title, image_url
 
 
 def download_txt(response, filename, folder='books/'):
@@ -28,8 +31,18 @@ def download_txt(response, filename, folder='books/'):
         file.write(book_text)
 
 
+def download_image(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    filename = urlsplit(url).path.split('/')[-1]
+    filepath = os.path.join('images', filename)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+
 def main():
     Path('books').mkdir(parents=True, exist_ok=True)
+    Path('images').mkdir(parents=True, exist_ok=True)
     book_text_url_template = 'https://tululu.org/txt.php?id='
     book_page_url_template = 'https://tululu.org/b'
     for i in range(1, 11):
@@ -39,9 +52,10 @@ def main():
         response.raise_for_status()
         try:
             check_for_redirect(response)
-            book_title = get_book_title(page_url)
+            book_title, image_url = get_book_details(page_url)
             filename = f'{i}. ' + book_title + '.txt'
-            download_txt(response, filename)
+            # download_txt(response, filename)
+            download_image(image_url)
         except requests.exceptions.HTTPError:
             pass
 
