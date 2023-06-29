046e37078cf51dda12dcdf168f560390245e85c7
free-flow-code <wildmuskaria8@gmail.com>
2023-06-29 15:30:07 +0400

    Added command line arguments.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -15,6 +15,9 @@ def parse_arguments():
     )
     parser.add_argument('--start_page', help='номер стартовой страницы', type=int, nargs='?', default=1)
     parser.add_argument('--end_page', help='номер конечной страницы', type=int, nargs='?', default=0)
+    parser.add_argument('--dest_folder', help='путь к каталогу с результатами парсинга', type=str, nargs='?', default=0)
+    parser.add_argument('--skip_imgs', help='не скачивать картинки', action='store_true')
+    parser.add_argument('--skip_txt', help='не скачивать книги', action='store_true')
     return parser.parse_args()
 
 
@@ -26,9 +29,6 @@ def get_page_books(url_template, response):
 
 
 def main():
-    Path('books').mkdir(parents=True, exist_ok=True)
-    Path('images').mkdir(parents=True, exist_ok=True)
-
     url_template = 'https://tululu.org/'
     category = 'l55/'
     book_text_url = 'https://tululu.org/txt.php'
@@ -37,7 +37,9 @@ def main():
     args = parse_arguments()
     start_page = args.start_page
     end_page = args.end_page
-    print(start_page, end_page)
+    dest_folder = args.dest_folder
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
 
     category_page_response = requests.get(urljoin(url_template, category))
     category_page_response.raise_for_status()
@@ -47,6 +49,14 @@ def main():
         soup = BeautifulSoup(category_page_response.text, 'lxml')
         max_category_page = int(soup.find_all(class_='npage')[-1].text)
         end_page = max_category_page
+
+    if dest_folder:
+        Path(os.path.join(dest_folder, 'books')).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(dest_folder, 'images')).mkdir(parents=True, exist_ok=True)
+    else:
+        dest_folder = '.'
+        Path('books').mkdir(parents=True, exist_ok=True)
+        Path('images').mkdir(parents=True, exist_ok=True)
 
     for page in range(start_page, end_page):
         if page == 1:
@@ -71,13 +81,17 @@ def main():
                 check_for_redirect(page_response)
                 page_details = parse_book_page(page_response)
 
-                filename = f'{book_id}. {page_details["book_title"]}.txt'
-                download_txt(file_response, filename)
-                download_image(page_details['image_url'])
+                if not skip_txt:
+                    filename = f'{book_id}. {page_details["book_title"]}.txt'
+                    download_txt(file_response, filename)
+                    page_details['book_path'] = os.path.join('books', filename)
 
-                page_details['image_url'] = os.path.join('images', page_details['image_url'].split('/')[-1])
-                page_details['book_path'] = os.path.join('books', filename)
-                with open('books_data.json', 'a', encoding='utf-8') as file:
+                if not skip_imgs:
+                    download_image(page_details['image_url'])
+                    page_details['image_url'] = os.path.join('images', page_details['image_url'].split('/')[-1])
+                del page_details['image_url']
+
+                with open(os.path.join(dest_folder, 'books_data.json'), 'a', encoding='utf-8') as file:
                     json.dump(page_details, file, ensure_ascii=False)
 
                 print(book_url)
