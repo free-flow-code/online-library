216985b8d538e7ae5db403ad983a97ce565d1bd2
free-flow-code <wildmuskaria8@gmail.com>
2023-06-30 14:09:53 +0400

    String addition has been replaced by a template.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -54,7 +54,7 @@ def main():
         Path('images').mkdir(parents=True, exist_ok=True)
 
     for page in range(start_page, end_page):
-        page_response = requests.get(urljoin(url_template, category + str(page)))
+        page_response = requests.get(urljoin(url_template, f'{category}{str(page)}'))
         page_response.raise_for_status()
         book_urls += get_page_books(url_template, page_response)
 
