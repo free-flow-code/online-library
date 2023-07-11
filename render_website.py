6a0b231a20a399983a3bfb212d86bfe89f0a9176
free-flow-code <wildmuskaria8@gmail.com>
2023-07-11 23:04:11 +0400

    Add paginator.

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import os
 import json
+import math
 from livereload import Server
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -18,14 +19,20 @@ def on_reload():
 
     os.makedirs("pages", exist_ok=True)
     page = 1
+    books_per_page = 10
+    total_pages = [*range(1, math.ceil(len(books) / books_per_page) + 1)]
     for index, value in enumerate(books):
-        if index and index % 10 == 0:
-            rendered_page = template.render(books=books[index - 10:index])
-            del books[index - 10:index]
+        if index and index % books_per_page == 0:
+            rendered_page = template.render(
+                books=books[index - books_per_page:index],
+                page_id=page,
+                total_pages=total_pages
+            )
+            del books[index - books_per_page:index]
             with open(f'./pages/index{page}.html', 'w', encoding="utf8") as file:
                 file.write(rendered_page)
             page += 1
-    rendered_page = template.render(books=books)
+    rendered_page = template.render(books=books, page_id=page, total_pages=total_pages)
     with open(f'./pages/index{page}.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
@@ -34,7 +41,7 @@ def main():
     on_reload()
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve(root='pages', default_filename='index1.html')
+    server.serve(root='./pages', default_filename='index1.html')
 
 
 if __name__ == '__main__':
